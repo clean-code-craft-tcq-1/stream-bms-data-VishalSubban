15ac1bc96bc2824015cec2ece63f14bf43610b21
Vishal Subban (MS/EDI2-XC) <ivh1cob@bosch.com>
2021-06-01 15:21:42 +0530

    Sender Commit

--- src/bms-exec.c
+++ src/bms-exec.c
@@ -28,5 +28,5 @@ void BmsDataPrint(float * BMSData)
 
 float BmsDataGenerator(float min, float max)
 {
-    return (rand() % (max - min + 1)) + min;
+    return ((float)rand() % (max - min + 1)) + min;
 }
