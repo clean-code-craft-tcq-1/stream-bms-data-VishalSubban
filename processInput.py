4e578e720e5e3f06fd7bb151ad1c93e8d2133629
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-06-21 17:10:09 +0530

    Create processInput.py

---
+++ processInput.py
@@ -0,0 +1,20 @@
+import receiver_calculation
+import sys
+
+def getSenderInput():
+  temp = []
+  soc = []
+  for i in sys.stdin:
+    list_temp,list_soc = formatInputStream(i)
+    temp.extend(list_temp)
+    soc.extend(list_soc)
+    if len(temp) >= 5 and len(soc) >=5:
+       receiver_calculation.calculateAvg(temp,soc)
+
+def formatInputStream(input_data):
+  input_vals = input_data.split(';')
+  list_temp = input_vals[0]
+  list_soc = input_vals[1]
+    return list_temp,list_soc
+    
+getSenderInput()
