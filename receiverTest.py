b8de1b7b2645a3959465d772fb970fa16a61b06f
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-06-21 18:57:33 +0530

    Update receiverTest.py

--- receiverTest.py
+++ receiverTest.py
@@ -1,25 +1,25 @@
 import unittest
-import receiver_calculate
+import receiver_calculation
 class ReceiverTest(unittest.TestCase):
      
     def testMinTemp(self):
-        self.assertTrue(receiver_calculate.calculateMin({'temperature':[20,19,1,15,18,90,60,80,70,50]},'temperature') == 1)
+        self.assertTrue(receiver_calculation.calculateMin({'temperature':[20,19,1,15,18,90,60,80,70,50]},'temperature') == 1)
         
     def testMaxTemp(self):
-        self.assertTrue(receiver_calculate.calculateMax({'temperature':[20,19,10,15,18,96,60,80,70,50]},'temperature') == 96)  
+        self.assertTrue(receiver_calculation.calculateMax({'temperature':[20,19,10,15,18,96,60,80,70,50]},'temperature') == 96)  
         
     def testMinSOC(self):
-        self.assertTrue(receiver_calculate.calculateMin({'soc':[70,40,25,9,90,98,88,77,66,55]},'soc') == 9)
-        self.assertTrue(receiver_calculate.calculateMin({'temperature':[20,19,10,15,18,96,60,80,70,50],'soc':[70,40,25,9,90,98,88,77,66,55]},'temperature')==10)  
+        self.assertTrue(receiver_calculation.calculateMin({'soc':[70,40,25,9,90,98,88,77,66,55]},'soc') == 9)
+        self.assertTrue(receiver_calculation.calculateMin({'temperature':[20,19,10,15,18,96,60,80,70,50],'soc':[70,40,25,9,90,98,88,77,66,55]},'temperature')==10)  
     
     def testMaxSOC(self):
-        self.assertTrue(receiver_calculate.calculateMax({'soc':[70,40,20,99,90,98,88,77,66,55]},'soc') == 99)
+        self.assertTrue(receiver_calculation.calculateMax({'soc':[70,40,20,99,90,98,88,77,66,55]},'soc') == 99)
         
     def testNone(self):
-        self.assertTrue(receiver_calculate.averageLastFive([])=='Length of the list is Zero')
+        self.assertTrue(receiver_calculation.averageLastFive([])=='Length of the list is Zero')
     
     def testAvgTempSOC(self):
-        self.assertTrue(receiver_calculate.averageLastFive([93,90,94,57,63,69,70,51,85,96]) == 74.2)
+        self.assertTrue(receiver_calculation.averageLastFive([93,90,94,57,63,69,70,51,85,96]) == 74.2)
         
 if __name__ == '__main__':
   unittest.main()
