9859bfbd4874e360cafee10c196edd51842c649b
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-06-21 18:30:32 +0530

    Create receiver_calculation.py

---
+++ receiver_calculation.py
@@ -0,0 +1,31 @@
+def calculateAvg(list_temp,list_soc):
+    final_list = {}
+    list_temp.sort()
+    list_soc.sort()
+    final_list['temperature'] = list_temp
+    final_list['soc'] =list_soc
+    print(final_list)
+    Output_to_console(final_list)
+    
+def calculateMin(data,parameter):
+    return min(data[parameter])
+def calculateMax(data,parameter):
+    return max(data[parameter])
+
+def displayOutput(input_list):
+    print('Max Temperature:', calculateMax(input_list,'temperature'))
+    print('Min Temperature:', calculateMin(input_list,'temperature'))
+    print('Max SOC:', calculateMax(input_list,'soc'))
+    print('Min SOC:', calculateMin(input_list,'soc'))
+    print('Average of last five values of temperature', averageLastFive(input_list['temperature']))
+    print('Average of last five values of soc', averageLastFive(input_list['soc']))
+    
+def averageLastFive(input_list):
+    sum_of_Vals = 0
+    length = len(input_list)
+    if length !=0:
+        for i in range(-1,-6,-1):
+            sum_of_Vals += input_lisy[i]
+        return sum_of_Vals/5
+    else:
+        return "Length of the list is Zero"
